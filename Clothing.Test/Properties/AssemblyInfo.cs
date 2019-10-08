29daa2d34da96756951324b4cfe65b318d2bc1c7
sheelasenga <54633529+sheelasenga@users.noreply.github.com>
2019-10-08 17:01:11 +0100

    Add files via upload

---
+++ Clothing.Test/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("Clothing.Test")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("HP Inc.")]
+[assembly: AssemblyProduct("Clothing.Test")]
+[assembly: AssemblyCopyright("Copyright © HP Inc. 2019")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("cfe28a23-3b35-4f69-815b-dc3ad9b662db")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ Clothing.Test/UnitTest1.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using NUnit.Framework;
+using CountryAPI;
+
+namespace Clothing.Test
+  
+{
+
+    [TestFixture]
+    public class CountryServiceTest
+    {
+
+        CountryCode countrycode = new CountryCode();
+
+        public CountryServiceTest()
+        {
+            countrycode.GetCountry("Andorra");
+        }
+        [Test]
+        public void Status()
+        {
+            Assert.AreEqual("200", countrycode.CountryResponseContent["code"].ToString());
+        }
+
+        [Test]
+        public void Country()
+        {
+            Assert.AreEqual("Andorra", countrycode.CountryResponseContent["result"][0]["name"].ToString());
+        }
+
+        [Test]
+        public void CountryCode()
+        {
+            Assert.AreEqual("AD", countrycode.CountryResponseContent["result"][0]["code"].ToString());
+        }
+
+
+        [Test]
+        public void CountryState()
+        {
+            Assert.AreEqual("", countrycode.CountryResponseContent["result"][0]["states"].ToString());
+        }
+
+        [Test]
+        public void NotEqual()
+        {
+            Assert.AreNotEqual("Andorra", countrycode.CountryResponseContent["result"][1]["name"].ToString());
+        }
+
+        [Test]
+        public void Header()
+        {
+         // Assert.AreEqual("Ok", countrycode.CountryResponseContent.Headers.Location.AbsoluteUri);
+        }
+    }
+}
